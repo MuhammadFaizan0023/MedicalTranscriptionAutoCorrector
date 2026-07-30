b86e877aad129b9f3b24c72094c33f989eee49c1
Muhammad Faizan <145048828+MuhammadFaizan0023@users.noreply.github.com>
2026-07-30 18:15:20 +0500

    Delete MT_system directory

--- MT_system/test.py
+++
@@ -1,6 +0,0 @@
-from pipeline import AutocorrectPipeline
-
-pipeline = AutocorrectPipeline(vocab_freq_path="./Data Files/merged_vocab_updated.pkl")
-vocab_set = "./Data Files/merged_vocab_updated.pkl"
-test = "then, 1% lidocaine was ued for anesthesia."
-print(pipeline.correct_sentence(test,vocab_set))
